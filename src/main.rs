8f1a7287dad3c15527618019abcae8c85a8d8181
John Aughey <jha@aughey.com>
2025-01-01 20:39:37 +0000

    asdf

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::{collections::HashSet, vec};
+
+fn print_grid<'a, INNER>(grid: impl IntoIterator<Item = INNER>)
+where
+    INNER: IntoIterator<Item = char>,
+{
+    for row in grid {
+        for cell in row {
+            print!("{}", cell);
+        }
+        println!();
+    }
+}
+
+fn print_grid_trait<'a, INNER>(grid: impl IntoIterator<Item = INNER>)
+where
+    INNER: IntoIterator<Item = char>,
+{
+    for row in grid {
+        for cell in row {
+            print!("{}", cell);
+        }
+        println!();
+    }
+}
+
+fn copied_inner_iter<'a, INNER, T>(
+    grid: impl IntoIterator<Item = INNER>,
+) -> impl Iterator<Item = impl Iterator<Item = T>>
+where
+    INNER: IntoIterator<Item = &'a T> + Clone,
+    T: 'a + Copy,
+{
+    grid.into_iter().map(|row| row.into_iter().cloned())
+}
+
+fn make_grid_from_sprase_grid(
+    sparse_grid: &HashSet<(usize, usize)>,
+    rows: usize,
+    cols: usize,
+) -> impl Iterator<Item = impl Iterator<Item = char> + '_> + '_ {
+    (0..rows).map(move |i| (0..cols).map(move |j| sparse_grid.get(&(i, j)).map_or('.', |_| 'X')))
+}
+
+fn main() {
+    let grid = [
+        ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', 'X', 'X', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', 'X', 'X', '.', '.', '.', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', 'X', 'X', '.', '.', '.', '.'],
+        ['.', '.', '.', '.', 'X', 'X', '.', '.', '.', '.'],
+    ];
+    println!("2D grid:");
+    print_grid(copied_inner_iter(&grid));
+
+    let vec_grid = vec![
+        vec!['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
+        vec!['.', 'X', 'X', '.', '.', '.', '.', '.', '.', '.'],
+        vec!['.', 'X', 'X', '.', '.', '.', '.', '.', '.', '.'],
+        vec!['.', '.', '.', '.', 'X', 'X', '.', '.', '.', '.'],
+        vec!['.', '.', '.', '.', 'X', 'X', '.', '.', '.', '.'],
+    ];
+    println!("2D vec grid:");
+    print_grid(copied_inner_iter(&vec_grid));
+
+    let sparse_grid = HashSet::<(usize, usize)>::from_iter([
+        (1, 1),
+        (1, 2),
+        (2, 1),
+        (2, 2),
+        (3, 4),
+        (3, 5),
+        (4, 4),
+        (4, 5),
+    ]);
+
+    let sparse_grid_iter = make_grid_from_sprase_grid(&sparse_grid, grid.len(), grid[0].len());
+
+    println!("Sparse grid:");
+    print_grid(sparse_grid_iter);
+}
